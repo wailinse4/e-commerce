0b7765caa204dc6a7757248dce4deabae0711e6c
wailinse4 <wailinse4@gmail.com>
2025-07-14 19:02:54 -0400

    feat: Add react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom"
 
 const App = () => {
   return (
-    <div className="text-blue-300">Hello, World!</div>
+    <Routes>
+      
+    </Routes>
   )
 }
 
