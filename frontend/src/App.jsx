b52e5e51389cb6b52815e230eed6bf73fb82e3de
wailinse4 <wailinse4@gmail.com>
2025-07-14 19:15:44 -0400

    feat: Implement signup ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
-import { Routes, Route } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import { Toaster } from "react-hot-toast"
 
-const App = () => {
-  return (
-    <div>
-      <Routes>  
+import SignupPage from "./pages/SignupPage"
 
-      </Routes>
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 text-gray-900 relative overflow-hidden">
+      <div className="relative z-50 pt-20">
+        <Routes>
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </div>
       <Toaster />
     </div>
-
   )
 }
 
