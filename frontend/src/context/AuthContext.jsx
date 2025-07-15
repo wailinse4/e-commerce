5041fb401c2111148a6f2711109ebc2cb287d58f
wailinse4 <wailinse4@gmail.com>
2025-07-15 14:24:45 -0400

    feat(auth): Implement  automatic auth check on mount

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import axiosInstance from "../config/axiosInstance.js"
 
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useState, useEffect } from "react"
 import { toast } from "react-hot-toast"
 
 export const AuthContext = createContext() 
@@ -9,19 +9,21 @@ export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
     const [isSigningUp, setIsSigningUp] = useState(false)
     const [isLoggingIn, setIsLoggingIn] = useState(false)
-    const [isCheckingAuth, setIsCheckingAuth] = useState(false)
+    const [isCheckingAuth, setIsCheckingAuth] = useState(true)
 
     const signup = async (fullName, email, password, confirmPassword) => {
         setIsSigningUp(true)
         try {
             const response = await axiosInstance.post("/auth/signup", { fullName, email, password, confirmPassword })
             setUser(response.data.data)
-        }
-        catch(error) {
+            toast.success("Signup successful")
+        } 
+        catch (error) {
             setUser(null)
-            console.log(error)
+            console.error(error)
+            toast.error("Signup failed")
             throw error
-        }
+        } 
         finally {
             setIsSigningUp(false)
         }
@@ -32,36 +34,50 @@ export const AuthProvider = ({ children }) => {
         try {
             const response = await axiosInstance.post("/auth/login", { email, password })
             setUser(response.data.data)
-        }
-        catch(error) {
+            toast.success("Login successful")
+        } 
+        catch (error) {
             setUser(null)
             console.error(error)
-            throw error 
-        }
+            toast.error("Login failed")
+            throw error
+        } 
         finally {
             setIsLoggingIn(false)
         }
-    }   
-    
+    }
+
     const checkAuth = async () => {
-        setIsCheckingAuth(true)
         try {
-            const response = await axiosInstance.get("/auth/check-auth")
+            const response = await axiosInstance.get('/auth/check-auth')
             setUser(response.data.data)
-        }
-        catch(error) {
+        } 
+        catch (error) {
+            console.error("Authentication check failed")
             setUser(null)
-            console.error(error)
-            throw error 
-        }
+        } 
         finally {
             setIsCheckingAuth(false)
         }
     }
 
+    useEffect(() => {
+        checkAuth()
+    }, [])
+
     return (
-        <AuthContext.Provider value={{ user, setUser, signup, isSigningUp, login, isLoggingIn, checkAuth, isCheckingAuth }}>
-            {children}
+        <AuthContext.Provider value={{ 
+            user, 
+            isSigningUp, 
+            setIsSigningUp, 
+            signup, 
+            isLoggingIn, 
+            setIsLoggingIn, 
+            login, 
+            isCheckingAuth, 
+            setIsCheckingAuth
+        }}>
+            { children }
         </AuthContext.Provider>
     )
 }
