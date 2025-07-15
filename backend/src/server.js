89e3c76819cd8a8fc6974d5555b8290f24078b13
wailinse4 <wailinse4@gmail.com>
2025-07-15 09:21:20 -0400

    fix(api): Configure CORS for credentials with frontend uURL and methods

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,11 @@ app.get("/", (req,res) => res.send("Hello, World!"))
 
 app.use(helmet())
 app.use(morgan("dev"))
-app.use(cors())
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+}))
 
 app.use(express.json({ limit: "50mb" }))
 app.use(express.urlencoded({ limit: "50mb", extended: true }))
