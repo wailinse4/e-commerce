9882122e75a6970199ddd70c04a58af5d495ad93
wailinse4 <wailinse4@gmail.com>
2025-07-16 09:05:09 -0400

    fix(auth): Update USER_PASS to EMAIL_PASS to match .env configuration file

--- backend/src/config/nodemailer.js
+++ backend/src/config/nodemailer.js
@@ -1,10 +1,13 @@
 
+import dotenv from "dotenv"
 import nodemailer from "nodemailer"
+
+dotenv.config()
 
 const transporter = nodemailer.createTransport({
     service: "gmail", 
     auth: {
-        user: process.env.USER_PASS, 
+        user: process.env.EMAIL_USER, 
         pass: process.env.EMAIL_PASS, 
     }
 })
