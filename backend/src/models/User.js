6794a02fbcbf47772b3a514b01b1c8ec62f48631
wailinse4 <wailinse4@gmail.com>
2025-07-15 22:02:40 -0400

    feat(auth): Add email verification fields to user model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -4,6 +4,10 @@ const userSchema = new mongoose.Schema({
     fullName: { type: String, required: true, trim: true }, 
     email: { type: String, required: true, trim: true, lowercase: true, unique: true },
     password: { type: String, required: true, minlength: 6 }, 
+
+    verificationCode: { type: String }, 
+    verificationCodeExpiresAt: { type: Date }, 
+    isVerified: { type: Boolean, default: false },
 }, { timestamps: true })
 
 const User = new mongoose.model("User", userSchema)
